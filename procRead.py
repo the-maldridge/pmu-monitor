1d059fe7969f7c9bbfa0c8413ce4a756f5a4caca
Michael Aldridge <maldridge@Jane.p0rtalNet.local>
2013-08-04 22:54:56 -0500

    changed behavior to open files for each read, updated methods accordingly

--- procRead.py
+++ procRead.py
@@ -1,31 +1,30 @@
 class power():
   def __init__(self, battery="/proc/pmu/battery_0", general="/proc/pmu/info"):
-    self.battery=open(battery)
-    self.general=open(general)
+    self.battery=battery
+    self.general=general
 
-  def update(self):
-    self.battery.seek(0)
-    self.general.seek(0)
-    bat_data = self.battery.read()
-    gen_data = self.general.read()
-    print bat_data, gen_data
-    return self.__parse(bat_data), self.__parse(gen_data)
+  def printdata(self):
+    data = self.__update()
+    print data["AC Power"], data["time rem."]
 
-  def release(self):
-    self.battery.close()
-    self.general.close()
+  def __update(self):
+    batfile = open(self.battery)
+    genfile = open(self.general)
+    bat_data = batfile.read()
+    gen_data = genfile.read()
+    batfile.close()
+    genfile.close()
+    out = {}
+    out =  self.__parse(bat_data).items() + self.__parse(gen_data).items()
+    return out
 
   def __parse(self, data):
     out = {}
-    print data
     for line in data.split("\n"):
       if line in ["\n", ""]: 
-        print line
         pass
       else:
-        print line
         unpacked = line.split(":")
-        print unpacked
         key = unpacked[0].strip(" ")
         val = unpacked[1].strip(" ")
         out[key]=val
